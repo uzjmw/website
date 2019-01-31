29a1a37f459961aa4e588e728f7ec105a98d9c58
uzjmw <47223191+uzjmw@users.noreply.github.com>
2019-01-31 18:50:56 +0100

    fggf
    gffg

---
+++ Einblendenscript.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function druck(){
+    document.getElementById("versteckt").style.display = "none";
+}
+
+function druck2(){
+    document.getElementById("versteckt").style.display = "block";
+}
