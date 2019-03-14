17b7c65d4812a84eaecb353f7303dacab3e8b030
uzjmw <47223191+uzjmw@users.noreply.github.com>
2019-03-14 23:46:13 +0100

    Update Einblendenscript.js

--- Einblendenscript.js
+++ Einblendenscript.js
@@ -19,14 +19,8 @@ var video = document.getElementById('video');
 
 function camera(){
 // Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }}).then(
-    //navigator.mediaDevices.getUserMedia({ video: true}).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
-	 start();
-	    
-    );
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }})) {
+	 start(); 
 }
 }
 
