887f4b2b94bd46bd5689b96e5b3baf7af2ede686
uzjmw <47223191+uzjmw@users.noreply.github.com>
2019-03-14 23:35:06 +0100

    Update Einblendenscript.js

--- Einblendenscript.js
+++ Einblendenscript.js
@@ -25,8 +25,6 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     //navigator.mediaDevices.getUserMedia({ video: true}).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
 	 start();
-        video.srcObject = stream;
-        video.play();
 	    
     });
 }
